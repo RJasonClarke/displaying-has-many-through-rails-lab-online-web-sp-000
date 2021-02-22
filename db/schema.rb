f8a9d61006db181e9ae683f345309f409254b3ed
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-22 17:30:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20201204032132) do
+
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
     t.integer  "patient_id"
