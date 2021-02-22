3c84027dbfa4b7d36d6c57c1c558e0ca70a18471
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-22 17:28:56 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,10 @@
-class AppointmentsController < ApplicationController
+class AppointmentsController < ApplicationController 
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
 
-  def show
-    @appointment = Appointment.find(params[:id])
-  end
+    def new
+        @appointment = Appointment.new
+      end
 
 end
