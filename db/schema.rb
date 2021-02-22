ca1ebf10c238edd194ffa08920fe79ac93b63e89
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-22 17:32:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201204032132) do
+ActiveRecord::Schema.define(version: 20210221043542) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
     t.integer  "patient_id"
     t.integer  "doctor_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "department"
   end
 
   create_table "patients", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string  "name"
+    t.integer "age"
   end
 
 end
