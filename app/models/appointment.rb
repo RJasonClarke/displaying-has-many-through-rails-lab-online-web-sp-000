81678088b27a85a1b313ee08f3f6494117ade161
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-22 17:29:31 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
-class Appointment < ApplicationRecord
-  belongs_to :doctor
-  belongs_to :patient
-end
+class Appointment < ActiveRecord::Base
+    belongs_to :patient
+    belongs_to :doctor
+
+    def date_format
+        self.appointment_datetime.strftime('%B %d, %Y at %H:%M')
+    end
+
+end 
